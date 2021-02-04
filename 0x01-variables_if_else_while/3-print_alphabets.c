cb9c466ac1f57c244c733cbbca7b21fc05ca6667
luismelendez94 <2709@holbertonschool.com>
2021-02-04 21:02:10 +0000

    Print Alphabet lower and Upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print Letters
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
