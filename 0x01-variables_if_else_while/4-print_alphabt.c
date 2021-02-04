ae298e60059e9ed76cfc1dcd3a44f85c2bdc4da9
luismelendez94 <2709@holbertonschool.com>
2021-02-04 21:15:55 +0000

    Alphabet without Q or E

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print Letters
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		if ((c != 'q') && (c != 'e'))
+			putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
