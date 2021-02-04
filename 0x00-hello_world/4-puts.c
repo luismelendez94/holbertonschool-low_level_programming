0cb972d72d8288f1e2633f26cc419af46b631634
luismelendez94 <2709@holbertonschool.com>
2021-02-04 03:19:12 +0000

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+/* Program to print a string */
+
 int main()
 {
-    
-    puts("\"Programming is like building a multilingual puzzle");
-    
-    return 0;
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return 0;
 }
