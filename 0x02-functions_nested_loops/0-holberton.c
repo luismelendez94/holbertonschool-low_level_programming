c7260cbd7a4cad30e2dd9d41953fa5a65dbbdced
luismelendez94 <2709@holbertonschool.com>
2021-02-08 20:15:36 +0000

    Nested Loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - This program prints "Holberton"
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	char c;
+
+	
+	_putchar(c);
+
+	return(0);
+}
