912cbbf7535a4b4047ce94e2353e080ca7574ac9
luismelendez94 <2709@holbertonschool.com>
2021-02-09 02:30:34 +0000

    _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,10 +8,16 @@
  */
 int main(void)
 {
-	char c;
-
-	
-	_putchar(c);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
 	return(0);
 }
