e0b0340e06cd0c5324e7f9e73a157f72594c8326
luismelendez94 <2709@holbertonschool.com>
2021-02-10 03:15:38 +0000

    Print Last Digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * print_last_digit - Find the last digit of a number
+ * @n: Number to be check
+ *
+ * Return: the answer
+ */
+int print_last_digit(int n)
+{
+	return (n % 10 + '0');
+}
