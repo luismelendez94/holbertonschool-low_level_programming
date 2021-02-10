04fe1cd0f87122b9c952cb7357cc2038c2960778
luismelendez94 <2709@holbertonschool.com>
2021-02-10 02:47:52 +0000

    Largest Number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-
-
-	if (a > b && a > c)
+	int largest;
+	
+	if (a >= b && a >= c)
 	{
-		return (a);
+		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b >= a && b >= c)
 	{
-		return (b);
+		largest = b;
 	}
 	else
 	{
-		return (c);
+		largest = c;
 	}
 
-
+	return (largest);
 }
