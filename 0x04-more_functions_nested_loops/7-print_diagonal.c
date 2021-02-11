fc08ec3327d27b59c05d2da8e2d4f60b8329a676
luismelendez94 <2709@holbertonschool.com>
2021-02-11 20:56:56 +0000

    Print Diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Print Diagonal Line
+ * @n: The character to specify how long
+ *
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 0; j < i - 1; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
