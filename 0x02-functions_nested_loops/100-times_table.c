cd76518cc0af3804cc97cfa1a3604c2ea6618626
luismelendez94 <2709@holbertonschool.com>
2021-02-11 03:56:17 +0000

    Times Table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,16 +1,41 @@
+#include <stdlib.h>
 #include "holberton.h"
 
 void print_times_table(int n)
 {
 	int i;
+	int j;
+	int r;
+	int r1;
 
-	if (n > 0 && n < 15)
+	if (n < 0 && n > 15)
 	{
-		return (0);
+		return;
 	}
 
 	for (i = 0; i <= n; i++)
 	{
-		
+		for (j = 0; j <= n; j++)
+		{
+			r = i * j;
+			r1 = ((r / 10) + '0');
+
+			if (r1 != '0')
+			{
+				_putchar(r1);
+			}
+			else if (j > 0)
+			{
+				_putchar(' ');
+			}
+			_putchar((r % 10) + '0');
+
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
 	}
 }
