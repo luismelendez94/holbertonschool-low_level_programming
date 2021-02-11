dafe58d2a7c260b134d54f2ea16a5c60da6b801b
luismelendez94 <2709@holbertonschool.com>
2021-02-11 17:36:56 +0000

    More Numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - Print Numbers from 0 to 14, 10 times
+ *
+ */
+void more_numbers(void)
+{
+	int i, veces = 1;
+
+	while (veces <= 10)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+			else
+				_putchar(i + '0');
+		}
+		_putchar('\n');
+		veces++;
+	}
+}
