9709523860f59000412b423b3a1f0247f6352cea
luismelendez94 <2709@holbertonschool.com>
2021-02-11 00:27:53 +0000

    Is Lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,8 +9,14 @@
  */
 int _islower(int c)
 {
-	if (islower(c))
-		return (1);
-	else
-		return (0);
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
