11ee9996f44c726a8df306d02599fba71829c5bf
luismelendez94 <2709@holbertonschool.com>
2021-02-11 21:09:41 +0000

    Print Square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,15 +5,15 @@
  * @size: The character to specify the size
  *
  */
-void print_square(int n)
+void print_square(int size)
 {
 	int i, j;
 
-	if (n > 0)
+	if (size > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j < size; j++)
 				_putchar('#');
 			_putchar('\n');
 		}
