d6e5c61b0e9e66fba971a5c499adf5835bc74bed
luismelendez94 <2709@holbertonschool.com>
2021-02-15 18:34:23 +0000

    Puts Half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,12 +14,11 @@ void puts_half(char *str)
 
 	if (len % 2 == 0)
 		n = len / 2;
-	else
+	if (len % 2 != 0)
 		n = (len - 1) / 2;
 
 	for (i = n; i < len; i++)
 	{
 		_putchar(str[i]);
 	}
-	_putchar('\n');
-}
+	_putchar('\n');}
