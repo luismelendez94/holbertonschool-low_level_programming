9140b11b521cfa5228f77a8f0049fe221683a9ae
luismelendez94 <2709@holbertonschool.com>
2021-02-19 23:51:24 +0000

    Print Number

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_number - Print integer
+ * @n: Number to be printed
+ *
+ */
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		x = -n;
+		_putchar('-');
+	}
+	else
+		x = n;
+
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
+
+	_putchar(x % 10 + '0');
+}
