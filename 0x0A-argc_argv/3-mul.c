1836075fd220786e17a7cff254814a6ffcf44df3
luismelendez94 <2709@holbertonschool.com>
2021-03-01 21:33:11 +0000

    Multiply 2 Numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * main - Multiply 2 Integers
+ * @argc: Argument count
+ * @argv: Contains the number of arguments
+ *
+ * Return: cero
+ */
+int main(int argc, char *argv[])
+{
+	int mult = 0;
+
+	if (argc < 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+
+	mult = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mult);
+
+	return (0);
+}
