16a18c9e759ed934adde242d253ec667f11460aa
luismelendez94 <2709@holbertonschool.com>
2021-03-03 03:15:21 +0000

    Concatenate two string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,11 @@ char *str_concat(char *s1, char *s2)
 	int i, j, sum = 0, len1 = 0, len2 = 0;
 	char *str;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	for (i = 0; s1[i] != '\0'; i++)
 		len1++;
 	for (j = 0; s2[j] != '\0'; j++)
