6cb2127384efe482c39a3228544537cfc13ccb06
luismelendez94 <2709@holbertonschool.com>
2021-03-05 01:31:11 +0000

    Concatenates two strings

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,8 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *pointer;
+	char *pointer;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -18,6 +19,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (pointer == NULL)
 		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		pointer[i] = 0;
 
 	return (pointer);
 }
