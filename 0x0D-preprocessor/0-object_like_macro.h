4ea91541aba9c2eb650895dae337e624bffb49c5
luismelendez94 <2709@holbertonschool.com>
2021-03-09 01:08:13 +0000

    Object-Like Macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+
+#endif /* OBJECT_LIKE_MACRO_H */
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+Lets see some **Preprocessor** in C
