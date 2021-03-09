47df02f94af22bff4febe1151d197cbce0c37de2
luismelendez94 <2709@holbertonschool.com>
2021-03-09 02:14:57 +0000

    Function Like Macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+
+#endif /* FUNCTION_LIKE_MACRO_H */
