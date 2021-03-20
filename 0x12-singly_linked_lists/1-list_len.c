63cb93b8799edb0091c0d712373e8e7b48a69a40
luismelendez94 <2709@holbertonschool.com>
2021-03-20 01:49:28 +0000

    Number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - Get the elements on the list
+ * @h: Pointer to the first node in the list
+ *
+ * Return: Number of nodes in the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
