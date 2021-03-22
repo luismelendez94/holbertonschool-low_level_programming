d43c6488773d23b3671900428c0b5898311ef91c
luismelendez94 <2709@holbertonschool.com>
2021-03-22 23:41:29 +0000

    Free List of Integers 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Free the elements of a Linked List
+ * @head: Pointer to the first element
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	*head = NULL;
+}
