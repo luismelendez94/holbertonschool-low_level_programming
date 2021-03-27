be00b14df07513feeba295a1760fb81728c5ed39
luismelendez94 <2709@holbertonschool.com>
2021-03-27 02:43:58 +0000

    Get the bit value

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * get_bit - Get the value of a bit at a given index
+ * @n: Number to be verify
+ * @index: The index of the bit
+ *
+ * Return: The value of the bit at index, -1 if an error ocurred
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return ((n >> index) & 1);
+}
