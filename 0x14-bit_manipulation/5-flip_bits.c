08b878c937ba1177d89dc14de5fcc969831f4e24
luismelendez94 <2709@holbertonschool.com>
2021-03-27 03:57:15 +0000

    Adding file

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - Flip number of bits to another number
+ * @n: First number to be verify
+ * @m: Second number to be verify
+ *
+ * Return: The number of bits needed to flip to get another number
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int number = 0;
+
+	return (number);
+}
