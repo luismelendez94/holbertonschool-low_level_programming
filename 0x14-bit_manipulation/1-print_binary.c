5ea3995e5e82189918d9e5b85aaedf93ef111a4f
luismelendez94 <2709@holbertonschool.com>
2021-03-27 00:14:31 +0000

    Print Binary Number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,22 +7,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i, k;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	for (i = 31; i >= 0; i--)
-	{
-		k = n >> i;
-
-		if (k & 1)
-			_putchar('1');
-		else
-			_putchar('0');
-	}
-
-/*
- * if (n > 1)
- *		print_binary(n >> 1);
- *
- *	_putchar('0' + (n & 1));
- */
+	_putchar('0' + (n & 1));
 }
