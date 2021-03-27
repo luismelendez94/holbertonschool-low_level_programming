d987202071fa831365393a6874821463e96b5bd1
luismelendez94 <2709@holbertonschool.com>
2021-03-27 03:25:47 +0000

    Set bit to 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * set_bit - Get the value of a bit at a given index
+ * @n: Pointer to a number to be verify
+ * @index: The index of the bit
+ *
+ * Return: The value of the bit at index, -1 if an error ocurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	*n ^= (1 << index);
+
+	return (1);
+}
