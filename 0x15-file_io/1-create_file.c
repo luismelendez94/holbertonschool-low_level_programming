eb596aa905a0f95b38f956d57cee723250817b10
luismelendez94 <2709@holbertonschool.com>
2021-03-31 02:24:10 +0000

    Create a File

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,6 +9,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	int openFile, writeFile, len;
+
 	if (filename == NULL)
 		return (-1);
+
+	openFile = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	if (openFile == -1)
+		return (-1);
+
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+
+	writeFile = write(openFile, text_content, len);
+	if (writeFile == -1)
+		return (-1);
+
+	close(openFile);
+
+	return (1);
 }
