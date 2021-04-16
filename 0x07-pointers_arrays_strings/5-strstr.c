4764bd709672c464d6c42bc134c725ea0803dbce
luismelendez94 <2709@holbertonschool.com>
2021-04-16 19:00:24 +0000

    Editing code

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strstr - Searches a string for any of a set of bytes
+ * @haystack: String to be verify
+ * @needle: String to match bytes
+ *
+ * Return: String searched
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (needle[j + 1] == '\0')
+					return (haystack + i);
+			}
+		}
+	}
+	return (NULL);
+}
