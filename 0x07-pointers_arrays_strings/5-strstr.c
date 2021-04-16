907de8e0d48579a3e3ed881bb8d3fe39a82b38c3
luismelendez94 <2709@holbertonschool.com>
2021-04-16 19:17:19 +0000

    Adding if statement

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,14 +11,18 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
+	if (*needle == 0)
+		return (haystack);
+
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		if (haystack[i] == needle[0])
 		{
 			for (j = 0; needle[j] != '\0'; j++)
 			{
-				if (needle[j + 1] == '\0')
-					return (haystack + i);
+				if (needle[j] == haystack[i + j])
+					if (needle[j + 1] == '\0')
+						return (haystack + i);
 			}
 		}
 	}
