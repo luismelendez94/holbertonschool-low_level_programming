2a083f4ce90ebba46cfdb1b110cb222118aa7722
luismelendez94 <2709@holbertonschool.com>
2021-05-27 23:22:02 +0000

    Index of a Key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,19 @@
-#include "hash_tables.c"
+#include "hash_tables.h"
 
-/**/
+/**
+ * key_index - Gives you the index of a key
+ * @key: A string that can be used as the key
+ * @size: Size of the array of the hash table
+ *
+ * Return: Index at wich the key/value pair
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
-{}
+{
+	unsigned long int index = 0;
+
+	if (size < 1)
+		return (0);
+
+	index = hash_djb2(key) % size;
+	return (index);
+}
