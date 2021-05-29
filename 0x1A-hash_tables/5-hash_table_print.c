0da80cf829293e2c13ef72be6886ebc4faef41be
luismelendez94 <2709@holbertonschool.com>
2021-05-29 03:10:03 +0000

    Print Table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,1 +1,26 @@
-void hash_table_print(const hash_table_t *ht);
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints hash table
+ * @ht: Pointer to the hash table
+ *
+ * Return: Nothing, just print
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *currentNode;
+	size_t i = 0;
+	char *comaSpace = "";
+
+	printf("{");
+	for (; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			currentNode = ht->array[i];
+			printf("%s'%s': '%s'", comaSpace, currentNode->key, currentNode->value);
+			comaSpace = ", ";
+		}
+	}
+	printf("}\n");
+}
