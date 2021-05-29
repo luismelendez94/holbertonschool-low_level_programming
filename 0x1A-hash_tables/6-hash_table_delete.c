da154e6a3ff802d01513b18430af9e6e2d70610b
luismelendez94 <2709@holbertonschool.com>
2021-05-29 03:55:04 +0000

    Delete table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,1 +1,39 @@
-void hash_table_delete(hash_table_t *ht);
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: Pointer to a hash table
+ *
+ * Return: Nothing, just deletes
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	size_t i = 0;
+	hash_node_t *currentNode;
+	hash_node_t *tempNode;
+
+	if (ht == NULL)
+		return;
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return;
+	}
+
+	while (i < ht->size)
+	{
+		tempNode = ht->array[i];
+		while (tempNode)
+		{
+			currentNode = tempNode->next;
+			free(tempNode->key);
+			free(tempNode->value);
+			free(tempNode);
+			tempNode = currentNode;
+		}
+		i++;
+	}
+
+	free(ht->array);
+	free(ht);
+}
