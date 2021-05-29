ba2539ec1b1ea50043e2e79af12a33dcab666652
luismelendez94 <2709@holbertonschool.com>
2021-05-29 02:19:27 +0000

    Update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -21,7 +21,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	if (arrayNode == NULL)
 		return (NULL);
-	
+
 	while (strcmp(arrayNode->key, key) != 0)
 		arrayNode = arrayNode->next;
 
