e4228a009086a9e21c4574572d190e2c2089fa16
luismelendez94 <2709@holbertonschool.com>
2021-09-18 03:58:04 +0000

    Task 0 and 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -23,5 +23,7 @@ typedef struct list_s
 } list_t;
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binary_recur(int *array, size_t start, size_t end, int value);
 
 #endif /* SEARCH_ALGOS_H */
