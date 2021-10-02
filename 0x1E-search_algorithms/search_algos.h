5a22003f5648f4974f20ea81723260df14f440ee
luismelendez94 <2709@holbertonschool.com>
2021-10-02 02:59:50 +0000

    Task 7

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <math.h>
 
 /**
  * struct list_s - singly linked list
@@ -25,5 +26,9 @@ typedef struct list_s
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_recur(int *array, size_t start, size_t end, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
